b4999d5f9b3a96b96b79f4bbffc95876b2187f5f
veerababug39 <veerababug39@gmail.com>
2023-08-15 11:16:19 +0000

    Destination Search Solution

---
+++ src/components/DestinationItem/index.js
@@ -0,0 +1,15 @@
+import './index.css'
+
+const DestinationItem = props => {
+  const {destinationDetails} = props
+  const {imgUrl, name} = destinationDetails
+
+  return (
+    <li className="container">
+      <img src={imgUrl} alt={name} className="image" />
+      <p className="paragraph">{name}</p>
+    </li>
+  )
+}
+
+export default DestinationItem
